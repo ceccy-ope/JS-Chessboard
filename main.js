274589ba45e492023f5d4ddce9fc0f66a891517d
Cecilia Muyide <83959507+ceccy-ope@users.noreply.github.com>
2023-05-02 23:58:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,33 @@
-const Pieces = document.querySelectorAll('img')
+const pieces = document.querySelectorAll('img')
 const squares = document.querySelectorAll('.squares')
 
-Pieces.forEach(piece => {
-piece.addEventListener('dragstart', dragStart )})
+pieces.forEach(piece => {
+  piece.addEventListener('dragstart', dragStart)
+})
 
+squares.forEach(square => {
+  square.addEventListener('dragover', dragOver)
+  square.addEventListener('drop', dragDrop)
+})
 
+let beingDragged
 
-    squares.forEach(squares => {
-    squares.addEventListener('dragover', dragOver)
-    squares.addEventListener('drop', dragDrop)})
-
-
-    let beingDragged
-
-    function dragStart(e){
-    beingDragged =e.target
+function dragStart(e) {
+  beingDragged = e.target
 }
 
+function dragOver(e) {
+  e.preventDefault()
+}
 
-    function dragOver(e){
-    e.preventDefault()
-    }
-        function dragDrop(e){
-            e.target.append(beingDragged)
-        }
+function dragDrop(e) {
+  const targetSquare = e.target
+
+  // Check if the target square already has a piece
+  if (targetSquare.querySelector('img')) {
+    return
+  }
+
+  // Append the piece to the target square
+  targetSquare.append(beingDragged)
+}
