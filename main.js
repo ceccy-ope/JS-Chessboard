9225e3f1111928accfc718b92226906bb5018667
Cecilia Muyide <83959507+ceccy-ope@users.noreply.github.com>
2023-05-03 01:09:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,26 @@
-const pieces = document.querySelectorAll('img');
-const squares = document.querySelectorAll('.squares');
+const Pieces = document.querySelectorAll('img')
+const squares = document.querySelectorAll('.squares')
 
-pieces.forEach(piece => {
-  piece.addEventListener('dragstart', dragStart);
-});
+Pieces.forEach(piece => {
+piece.addEventListener('dragstart', dragStart )})
 
-squares.forEach(square => {
-  square.addEventListener('dragover', dragOver);
-  square.addEventListener('drop', dragDrop);
-});
 
-let beingDragged;
 
-function dragStart(e) {
-  beingDragged = e.target;
+    squares.forEach(squares => {
+    squares.addEventListener('dragover', dragOver)
+    squares.addEventListener('drop', dragDrop)})
+
+
+    let beingDragged
+
+    function dragStart(e){
+    beingDragged =e.target
 }
 
-function dragOver(e) {
-  e.preventDefault();
-}
 
-function dragDrop(e) {
-  if (e.target.children.length === 0) {
-    e.target.appendChild(beingDragged);
-  }
-  else {
-    alert('Cannot place piece on top of another piece.');
-  }
-}
+    function dragOver(e){
+    e.preventDefault()
+    }
+        function dragDrop(e){
+            e.target.append(beingDragged)
+        }
